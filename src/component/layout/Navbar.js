a8fe43e8f84a44edf46dbb161012086b229e4a43
Mark Soriano <merksmanship@gmail.com>
2018-09-02 13:26:15 -0700

    build header

---
+++ src/component/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-dark bg-light mb-5">
+			<span className="nav-brand mb-0 h2 mx-auto bg-light rounded">
+				<i class="fas fa-music" /> Find Lyrics{' '}
+			</span>
+		</nav>
+	);
+};
+
+export default Navbar;
