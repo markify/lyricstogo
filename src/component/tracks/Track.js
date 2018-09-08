60f6986b3c69b7388175100cf3c75c30db8d4514
Mark Soriano <merksmanship@gmail.com>
2018-09-07 17:01:03 -0700

    add artist album and song list v3

--- src/component/tracks/Track.js
+++ src/component/tracks/Track.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Track = (props) => {
 	//destructuring instead of using props.tracks everywhere
 	const { track } = props;
@@ -9,6 +9,20 @@ const Track = (props) => {
 			<div className="card mb-4 shadow-sm">
 				<div className="card-body">
 					<h4> {track.artist_name}</h4>
+					<p className="card-text">
+						<strong>
+							<i className="far fa-play-circle" /> Track{' '}
+						</strong>{' '}
+						: {track.track_name}
+						<br />
+						<strong>
+							<i className="fas fa-headphones-alt" /> Album{' '}
+						</strong>{' '}
+						: {track.album_name}{' '}
+					</p>
+					<Link to={`lyrics/track/${track.track_id}`} className="btn btn-primary btn-block">
+						<i class="fas fa-angle-right" /> View Lyrics
+					</Link>
 				</div>
 			</div>
 		</div>
